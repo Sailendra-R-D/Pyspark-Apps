07ecfde887c5e7865bbb4494ced8174249a70e78
Sailendra R.D <sailendra.donthineni@gmail.com>
2019-10-05 15:37:18 +0530

    committing a pyspark app with dataframe

---
+++ spark_app.py
@@ -0,0 +1,32 @@
+# import required packages
+
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as fn
+
+if __name__ == "__main__":
+    print("HelloWorld Pyspark Application Started ...")
+    # initializing SparkSession
+    spark = SparkSession \
+        .builder \
+        .appName("PySpark App") \
+        .master("local[2]") \
+        .getOrCreate()
+
+    # Loading input json file
+    pokemonsDF = spark \
+        .read \
+        .option("inferSchema", "true") \
+        .json("Pokédex.json", multiLine=True)
+
+    pokemonsDF = pokemonsDF \
+        .select(fn
+                .explode(pokemonsDF.pokemon)
+                .alias('pokemon')) \
+        .select('pokemon.*')
+
+    pokemonsDF.printSchema()
+    pokemonsDF.show(truncate=False)
+
+    # stop the SparkSession object
+    spark.stop()
+    print("HelloWorld Pyspark Application Completed.")
